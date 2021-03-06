cf63254d0b130fcb8ed59f9dd9301ad6a94b3d38
R Bicer <rsn.bicer@hotmail.com>
2021-03-05 23:53:47 -0500

    R

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = 2a4a2ee44694f6746cf40ce5421e4027
+
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = AIzaSyAad2j8FEr7KVF0JOHx5X2xnOA0scHNKTE
